6b85d5f08cbd0229024b94ae2806920f9acd2dc8
pedroIgnacioM <pedro.ignaciom95@gmail.com>
2016-10-29 18:16:54 -0300

    interfaz

--- Proyecto 28_10_2016/src/Arreglo.java
+++ Proyecto 28_10_2016/src/Arreglo.java
@@ -1,5 +1,10 @@
 
 public interface Arreglo 
 {
-
+	public boolean agregar();
+	public boolean modificar();
+	public boolean eliminar();
+	public boolean contiene();
+	public Object busqueda();
+	
 }
