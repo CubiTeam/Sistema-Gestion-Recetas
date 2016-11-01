ca30204776d08b51f0cd26d837805b7e43a485e7
pedroIgnacioM <pedro.ignaciom95@gmail.com>
2016-11-01 17:55:57 -0300

    ordenando ventana eliminar usuario

--- Proyecto 28_10_2016/src/VentanaEliminarAlumno.java
+++ Proyecto 28_10_2016/src/VentanaEliminarAlumno.java
@@ -14,9 +14,12 @@ import java.awt.Font;
 public class VentanaEliminarAlumno extends JFrame {
 
 	private JPanel contentPane;
-	private Archivos archivo;
 
-	public VentanaEliminarAlumno(final Universidad universidad,final Alumno alumno, final VentanaDocente ventanaAnterior) {
+
+	public VentanaEliminarAlumno(final Universidad universidad,final Alumno alumno, final VentanaDocente ventanaAnterior) 
+	{
+		
+		
 		setTitle("Ventana Eliminar Alumno");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 383, 261);
@@ -26,8 +29,40 @@ public class VentanaEliminarAlumno extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		setResizable(false);
+		
+		JPanel panelDatosAlumno = new JPanel();
+		panelDatosAlumno.setBackground(new Color(51, 153, 255));
+		panelDatosAlumno.setBorder(new TitledBorder(new MatteBorder(1, 5, 1, 1, (Color) new Color(255, 255, 255)), "Datos alumno", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(255, 255, 255)));
+		panelDatosAlumno.setBounds(10, 13, 357, 168);
+		contentPane.add(panelDatosAlumno);
+		panelDatosAlumno.setLayout(null);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
 //------------------------------------------------------------------------------------------------------------------------
-													//BOTONES
+		//BOTONES
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//boton atras
 		JButton btnAtras = new JButton("Atras");
 		btnAtras.setFont(new Font("Century Gothic", Font.PLAIN, 12));
 		btnAtras.addActionListener(new ActionListener() {
@@ -40,6 +75,62 @@ public class VentanaEliminarAlumno extends JFrame {
 		btnAtras.setBounds(33, 192, 113, 30);
 		contentPane.add(btnAtras);
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//boton eliminar alumno
+		JButton btnEliminarAlumno = new JButton("Eliminar Alumno");
+		btnEliminarAlumno.setFont(new Font("Century Gothic", Font.PLAIN, 12));
+		btnEliminarAlumno.setBounds(196, 127, 151, 30);
+		panelDatosAlumno.add(btnEliminarAlumno);
+		btnEliminarAlumno.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)  {
+				if(universidad.getListaAlumnos().eliminar(alumno))
+				{
+					JOptionPane.showMessageDialog(null,"Datos eliminados exitosamente","Eliminar UsuarioEstandar",JOptionPane.INFORMATION_MESSAGE);
+					ventanaAnterior.setVisible(true);
+					setVisible(false);
+					dispose();
+				}
+				else
+					JOptionPane.showMessageDialog(null,"Ha ocurrido un error al eliminar","Eliminar UsuarioEstandar",JOptionPane.ERROR_MESSAGE);
+				}
+		});
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//boton salir
 		JButton btnSalir = new JButton("Salir");
 		btnSalir.setFont(new Font("Century Gothic", Font.PLAIN, 12));
 		btnSalir.addActionListener(new ActionListener() {
@@ -50,67 +141,69 @@ public class VentanaEliminarAlumno extends JFrame {
 		btnSalir.setBounds(230, 192, 113, 30);
 		contentPane.add(btnSalir);
 		
-		JPanel panelDatosAlumno = new JPanel();
-		panelDatosAlumno.setBackground(new Color(51, 153, 255));
-		panelDatosAlumno.setBorder(new TitledBorder(new MatteBorder(1, 5, 1, 1, (Color) new Color(255, 255, 255)), "Datos alumno", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(255, 255, 255)));
-		panelDatosAlumno.setBounds(10, 13, 357, 168);
-		contentPane.add(panelDatosAlumno);
-		panelDatosAlumno.setLayout(null);
 		
-			JLabel lblInformacionUsuario = new JLabel("Nombre :");
-			lblInformacionUsuario.setForeground(new Color(255, 255, 255));
-			lblInformacionUsuario.setFont(new Font("Century Gothic", Font.PLAIN, 12));
-			lblInformacionUsuario.setBounds(21, 21, 232, 14);
-			panelDatosAlumno.add(lblInformacionUsuario);
-			lblInformacionUsuario.setText("Nombre: "+alumno.getNombrePersona());
-			
-			JLabel lblRut = new JLabel("RUT: ");
-			lblRut.setForeground(new Color(255, 255, 255));
-			lblRut.setFont(new Font("Century Gothic", Font.PLAIN, 12));
-			lblRut.setBounds(256, 21, 101, 14);
-			panelDatosAlumno.add(lblRut);
-			lblRut.setText("RUT: "+alumno.getRut());
-			
-			JLabel lblCorreo = new JLabel("Correo: ");
-			lblCorreo.setFont(new Font("Century Gothic", Font.PLAIN, 12));
-			lblCorreo.setForeground(new Color(255, 255, 255));
-			lblCorreo.setBounds(21, 46, 326, 14);
-			panelDatosAlumno.add(lblCorreo);
-			lblCorreo.setText("Correo: "+alumno.getCorreo());
-			
-			JLabel lblDireccion = new JLabel("Direcci\u00F3n: ");
-			lblDireccion.setFont(new Font("Century Gothic", Font.PLAIN, 12));
-			lblDireccion.setForeground(new Color(255, 255, 255));
-			lblDireccion.setBounds(21, 71, 326, 14);
-			panelDatosAlumno.add(lblDireccion);
-			lblDireccion.setText("Dirección: "+alumno.getDireccion());
-			
-			JLabel lblEdad = new JLabel("Edad: ");
-			lblEdad.setFont(new Font("Century Gothic", Font.PLAIN, 12));
-			lblEdad.setForeground(new Color(255, 255, 255));
-			lblEdad.setBounds(21, 95, 151, 14);
-			panelDatosAlumno.add(lblEdad);
-			lblEdad.setText("Edad: "+alumno.getEdad());
-			
-			JLabel lblTelefono = new JLabel("Tel\u00E9fono: ");
-			lblTelefono.setFont(new Font("Century Gothic", Font.PLAIN, 12));
-			lblTelefono.setForeground(new Color(255, 255, 255));
-			lblTelefono.setBounds(21, 120, 175, 14);
-			panelDatosAlumno.add(lblTelefono);
-			lblTelefono.setText("Teléfono: "+alumno.getTelefono());
-			
-			JButton btnEliminarAlumno = new JButton("Eliminar Alumno");
-			btnEliminarAlumno.setFont(new Font("Century Gothic", Font.PLAIN, 12));
-			btnEliminarAlumno.setBounds(196, 127, 151, 30);
-			panelDatosAlumno.add(btnEliminarAlumno);
-			btnEliminarAlumno.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e)  {
-					if(universidad.getListaAlumnos().eliminar(alumno))
-						JOptionPane.showMessageDialog(null,"Datos eliminados exitosamente","Eliminar UsuarioEstandar",JOptionPane.INFORMATION_MESSAGE);
-					else
-						JOptionPane.showMessageDialog(null,"Ha ocurrido un error al eliminar","Eliminar UsuarioEstandar",JOptionPane.ERROR_MESSAGE);
-					}
-			});
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//---------------------------------------------------------------------------------------------------------------------------
+		
+		//JLabel
+	
+		JLabel lblInformacionUsuario = new JLabel("Nombre :");
+		lblInformacionUsuario.setForeground(new Color(255, 255, 255));
+		lblInformacionUsuario.setFont(new Font("Century Gothic", Font.PLAIN, 12));
+		lblInformacionUsuario.setBounds(21, 21, 232, 14);
+		panelDatosAlumno.add(lblInformacionUsuario);
+		lblInformacionUsuario.setText("Nombre: "+alumno.getNombrePersona());
+		
+		JLabel lblRut = new JLabel("RUT: ");
+		lblRut.setForeground(new Color(255, 255, 255));
+		lblRut.setFont(new Font("Century Gothic", Font.PLAIN, 12));
+		lblRut.setBounds(256, 21, 101, 14);
+		panelDatosAlumno.add(lblRut);
+		lblRut.setText("RUT: "+alumno.getRut());
+		
+		JLabel lblCorreo = new JLabel("Correo: ");
+		lblCorreo.setFont(new Font("Century Gothic", Font.PLAIN, 12));
+		lblCorreo.setForeground(new Color(255, 255, 255));
+		lblCorreo.setBounds(21, 46, 326, 14);
+		panelDatosAlumno.add(lblCorreo);
+		lblCorreo.setText("Correo: "+alumno.getCorreo());
+		
+		JLabel lblDireccion = new JLabel("Direcci\u00F3n: ");
+		lblDireccion.setFont(new Font("Century Gothic", Font.PLAIN, 12));
+		lblDireccion.setForeground(new Color(255, 255, 255));
+		lblDireccion.setBounds(21, 71, 326, 14);
+		panelDatosAlumno.add(lblDireccion);
+		lblDireccion.setText("Dirección: "+alumno.getDireccion());
+		
+		JLabel lblEdad = new JLabel("Edad: ");
+		lblEdad.setFont(new Font("Century Gothic", Font.PLAIN, 12));
+		lblEdad.setForeground(new Color(255, 255, 255));
+		lblEdad.setBounds(21, 95, 151, 14);
+		panelDatosAlumno.add(lblEdad);
+		lblEdad.setText("Edad: "+alumno.getEdad());
+		
+		JLabel lblTelefono = new JLabel("Tel\u00E9fono: ");
+		lblTelefono.setFont(new Font("Century Gothic", Font.PLAIN, 12));
+		lblTelefono.setForeground(new Color(255, 255, 255));
+		lblTelefono.setBounds(21, 120, 175, 14);
+		panelDatosAlumno.add(lblTelefono);
+		lblTelefono.setText("Teléfono: "+alumno.getTelefono());
+		
+		
 
 	}
 }
