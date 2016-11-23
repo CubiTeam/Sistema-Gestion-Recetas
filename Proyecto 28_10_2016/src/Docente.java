69eeee3f359b618ca900c068f45af8df17dab769
pedroIgnacioM <pedro.ignaciom95@gmail.com>
2016-11-23 14:20:48 -0300

    sobreescritura docente

--- Proyecto 28_10_2016/src/Docente.java
+++ Proyecto 28_10_2016/src/Docente.java
@@ -4,13 +4,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class Docente extends Persona{
+public class Docente extends Persona
+{
 
-	private String contrasena;
-	
 	public Docente(){
-		this ("","","","","",0,0);
-		this.contrasena = "";
+		this("","","","","",0,0,"");
 	}
 	
 	public Docente(String nombrePersona, String rut, String direccion, String correo, String sexo, int edad,
@@ -20,26 +18,20 @@ public class Docente extends Persona{
 
 	public Docente(String nombrePersona, String rut, String direccion, String correo, String sexo, int edad,
 			int telefono,String contrasena) {
-		super(nombrePersona, rut, direccion, correo, sexo, edad, telefono);
-		this.setContrasena(contrasena);
+		super(nombrePersona, rut, direccion, correo, sexo, edad, telefono,contrasena);
 		}
 	
-	public String getContrasena() {
-		return contrasena;
-	}
-
-	public void setContrasena(String contrasena) {
-		this.contrasena = contrasena;
-	}
-
-	public String generarContraseÒa(String rut) 
+	
+	
+	//se obtienen los ultimos digitos del rut y se convierte en la contrase√±a
+	public String generarContrase√±a(String rut) 
 	{
 		int pass,i,digito= 0, aux=0;
 		String contrasena;
 		pass = Integer.parseInt(rut);
 		
-		for(i=0;i<5;i++)				//se obtienen los ultimos digitos del rut
-		{								//y se convierte en la contraseÒa
+		for(i=0;i<6;i++)				
+		{								
 			digito = pass%10;
 			pass = pass/10;
 			aux = aux*10 + digito;
@@ -49,8 +41,9 @@ public class Docente extends Persona{
 		return contrasena;
 	}
 	
+	//valida que los datos del docente sean correctos
 	public boolean validarDocente(String rut, String password) 
-	{															//valida que los datos del docente sean correctos
+	{						
 		if(rut.equals(super.getRut()) && password.equals(getContrasena()))
 		{
 			return true;
