a02c03f8827c5331c2acc195ba2e16a65d6af507
pedroIgnacioM <pedro.ignaciom95@gmail.com>
2016-11-23 14:40:06 -0300

    sacando los set y get de contraseña

--- Proyecto 28_10_2016/src/Alumno.java
+++ Proyecto 28_10_2016/src/Alumno.java
@@ -1,48 +1,29 @@
 
 
 public class Alumno extends Persona{
-	private String contrasena;
 	private ListaReceta listaRecetas;
-	
-	
-	
-	
-	
 	
 	
 	
 	public Alumno()
 	{
-		this ("","","","","",0,0);
-		this.contrasena = "";
+		this ("","","","","",0,0,"");
 	}
-
 	
 	
+	public Alumno(String nombrePersona)
+	{
+		super(nombrePersona);
+	}
 	
 	
-	
-	
-	
-	
-
 	public Alumno(String nombrePersona, String rut, String direccion,
 			String correo, String sexo, int edad, int telefono, String contrasena)
 	{
-		super(nombrePersona, rut, direccion, correo, sexo, edad, telefono);
-		this.setContrasena(contrasena);
+		super(nombrePersona, rut, direccion, correo, sexo, edad, telefono,contrasena);
 		listaRecetas= new ListaReceta();
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	public Alumno(String nombrePersona, String rut, String direccion,
 			String correo, String sexo, int edad, int telefono)
@@ -51,147 +32,15 @@ public class Alumno extends Persona{
 		listaRecetas= new ListaReceta();
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	public String getContrasena() {
-		return contrasena;
-	}
-
-
-
-
-
-
-
-
-
-
-	public void setContrasena(String contrasena) {
-		this.contrasena = contrasena;
-	}
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	public ListaReceta getListaRecetas() {
 		return listaRecetas;
 	}
 
-
-
-
-
-
-
-
-
-
-	public void setListaRecetas(ListaReceta listaRecetas) {
+	public void setListaRecetas(ListaReceta listaRecetas) 
+	{
 		this.listaRecetas = listaRecetas;
 	}
 
-
-
-
-
-
-
-
-
-
-	public Alumno(String nombrePersona)
-	{
-		super(nombrePersona);
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	//se genera una contraseña estadar para el alumno
-		public String generarContraseña(String rut) 
-		{
-			int pass, i, digito = 0, aux = 0;
-			String contrasena;
-			pass = Integer.parseInt(rut);
-
-			for(i=0;i<5;i++)				//se obtienen los ultimos digitos del rut
-			{								//y se convierte en la contraseña
-				digito = pass%10;
-				pass = pass/10;
-				aux = aux*10 + digito;
-			}
-			this.contrasena = String.valueOf(aux);
-			contrasena = String.valueOf(aux);
-			return contrasena;
-		}
-		
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	@Override
 	public String toString()
 	{
